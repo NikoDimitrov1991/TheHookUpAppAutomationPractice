3b284b62158ab5b69e459ad3a8e3baae0cb887d9
nikolay.dimitrov <nikolay.dimitrov@delasport.com>
2024-10-04 18:02:50 +0300

    Applying Page Object Model by creating pageObjects package, AddRemovePage class, containing all the locators needed for the AddRemoveSteps steps and then refactoring AddRemoveSteps class

--- src/test/java/stepDefinitions/AddRemoveSteps.java
+++ src/test/java/stepDefinitions/AddRemoveSteps.java
@@ -3,51 +3,46 @@ package stepDefinitions;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+import pageObjects.AddRemovePage;
 import utils.TestBase;
-
-import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class AddRemoveSteps {
-    TestBase testBase;
+    final TestBase testBase;
+    final AddRemovePage addRemovePage;
 
 
     public AddRemoveSteps(TestBase testBase) {
         this.testBase = testBase;
+        this.addRemovePage = new AddRemovePage(testBase.driver);
     }
 
     @Given("User goes to AddRemove Elements page")
     public void user_goes_to_addRemove_elements_page() {
-        this.testBase.driver.findElement(By.cssSelector("a[href='/add_remove_elements/']")).click();
+        addRemovePage.clickAddRemoveLink();
         String currentUrl = testBase.driver.getCurrentUrl();
-        assertEquals("URL does not match", "https://the-internet.herokuapp.com/add_removeelements/", currentUrl);
+        assertEquals("URL does not match", "https://the-internet.herokuapp.com/add_remove_elements/", currentUrl);
     }
 
     @When("user clicks on the Add Element button")
     public void user_clicks_on_the_add_element_button() {
-        WebElement addElementButton = testBase.driver.findElement(By.xpath("//button[text()='Add Element']"));
-        addElementButton.click();
+        addRemovePage.clickAddElementButton();
     }
 
     @Then("a Delete button appears")
     public void a_delete_button_appears() {
-        List<WebElement> deleteButtons = testBase.driver.findElements(By.className("added-manually"));
-        assertTrue("Delete button is not displayed", deleteButtons.size() > 0);
+        assertTrue("Delete button is not displayed", addRemovePage.isDeleteButtonDisplayed());
     }
 
     @When("user clicks on the Delete button")
     public void user_clicks_on_the_delete_button() {
-        WebElement deleteButton = testBase.driver.findElement(By.className("added-manually"));
-        deleteButton.click();
+        addRemovePage.clickDeleteButton();
     }
 
     @Then("the Delete button should disappear")
     public void the_delete_button_should_disappear() {
-        List<WebElement> deleteButtons = testBase.driver.findElements(By.className("added-manually"));
-        assertEquals("Delete button did not disappear", 0, deleteButtons.size());
+        assertTrue("Delete button did not disappear", addRemovePage.isDeleteButtonAbsent());
     }
 }
